37ea5c820385e5674ba1dc912ba80f336852c8f2
Maximilian Clemens <m.clemens@mailbox.org>
2025-05-18 11:02:25 +0000

    get data from agent script

--- .docs/debuging.md
+++ .docs/debuging.md
@@ -1,4 +1,8 @@
 # Fehler in Rulesets
-omd restart apache
-tail -f  ~/var/log/apache/error_log
+* omd restart apache
+* tail -f  ~/var/log/apache/error_log
 
+# Manuell testen
+* cmk -v --debug -n fritzbox
+* cmk -v --debug --dump-agent  fritzbox
+* cmk -D fritzbox # anzeigen der datasources /hostconfig
---
+++ server_side_calls/special_agent.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from cmk.server_side_calls.v1 import SpecialAgentConfig, SpecialAgentCommand, noop_parser
+
+def _agent_arguments(params, host_config):
+    args = [
+        "--host", host_config.ipv4_config.address,
+        "--username", params.get("username", "x"),
+        "--password", params.get("password").unsafe()
+    ]
+    
+    yield SpecialAgentCommand(command_arguments=args)
+
+special_agent_fritzbox_smarthome = SpecialAgentConfig(
+    name="fritzbox_smarthome",
+    parameter_parser=noop_parser,
+    commands_function=_agent_arguments
+)
